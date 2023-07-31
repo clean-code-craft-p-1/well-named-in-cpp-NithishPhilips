54e3774a3ae5785e886f5379f73f2eec46775944
NithishPhilips <nithishkumar.punna@philips.com>
2023-07-31 21:55:54 +0530

    Second

--- ColorPair.cpp
+++ ColorPair.cpp
@@ -1,5 +1,10 @@
 #include "ColorPair.h"
 
+const char* MajorColorNames[] = { "White", "Red", "Black", "Yellow", "Violet" };
+int numberOfMajorColors = sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+const char* MinorColorNames[] = { "Blue", "Orange", "Green", "Brown", "Slate" };
+int numberOfMinorColors = sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+
 std::string TelCoColorCoder::ColorPair::ToString() {
 	std::string colorPairStr = MajorColorNames[majorColor];
 	colorPairStr += " ";
